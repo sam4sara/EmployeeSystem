0c8560229884e20f4e83f6e75bb376802cd216de
jay4sara <sam.javatechy@gmail.com>
2020-04-28 21:13:30 +0530

    Added Employee Bean in model package

--- src/main/java/com/sam/employee/model/Employee.java
+++ src/main/java/com/sam/employee/model/Employee.java
@@ -1,4 +1,17 @@
 package com.sam.employee.model;
 
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Employee {
+    private Integer employeeID;
+    private String EmployeeName;
+    private String age;
+    private String mobileNumber;
+    private String salary;
+    private String city;
 }
